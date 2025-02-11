35630f3cfe9491247395144e0951890aa31ef90a
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-02-11 11:24:46 -0300

    feat: create Menu.js

---
+++ src/models/Menu.js
@@ -0,0 +1,9 @@
+class Menu {
+    constructor(type, description, price) {
+        this.type = type;
+        this.description = description;
+        this.price = price;
+    }
+}
+
+module.exports = Menu;
